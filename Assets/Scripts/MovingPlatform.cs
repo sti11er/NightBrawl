f0ac8c01ac7357335ff8b3a185df3dc4c48ea29e
sti11er <64311703+sti11er@users.noreply.github.com>
2021-06-06 12:26:30 +0000

    Add files via upload

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    //public GameObject Platform;
+    public float speed = 0.001f;
+
+    void Update() 
+    {
+
+        Debug.Log(Time.deltaTime);
+        transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+    }
+}
